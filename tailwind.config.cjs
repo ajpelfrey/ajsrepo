a5305d50cc66a3b72b5cfb8f7e8446ee5f4e1515
Alexander Pelfrey <58529497+ajpelfrey@users.noreply.github.com>
2024-01-08 21:54:19 -0500

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -19,7 +19,7 @@ module.exports = {
         xs: "450px",
       },
       backgroundImage: {
-        "hero-pattern": "url('src/assets/backkkkk.png')",
+        "hero-pattern": "url('./assets/backkkkk.png')",
       },
     },
   },
