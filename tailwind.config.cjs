609f7974c5389cf2388158ee0c29ce337efda99d
ajpelfrey <apelfrey@ufl.edu>
2024-01-08 20:19:56 -0500

    hi

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -19,7 +19,7 @@ module.exports = {
         xs: "450px",
       },
       backgroundImage: {
-        "hero-pattern": "url('src/assets/backkk.png')",
+        "hero-pattern": "url('src/assets/tripguide.png')",
       },
     },
   },
