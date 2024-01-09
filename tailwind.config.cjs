cd237b9e7fe94f30bf5f6c27dc1a071c36679624
ajpelfrey <apelfrey@ufl.edu>
2024-01-08 22:24:05 -0500

    f

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -19,7 +19,7 @@ module.exports = {
         xs: "450px",
       },
       backgroundImage: {
-        'hero-pattern': 'linear-gradient(to right,#804dee,#11998e,#2f80ed,#434343,#000000 )',
+        'hero-pattern': 'linear-gradient(to right,#804dee,#11998e,#2f80ed )',
       },
     },
   },
