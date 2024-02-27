8a3a4c9df7695b895f6d94ab09cca750807220b1
ajpelfrey <apelfrey@ufl.edu>
2024-02-26 19:10:28 -0500

    fixing projects

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -44,8 +44,7 @@ const Navbar = () => {
         >
           <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
           <p className='text-white text-[18px] font-bold cursor-pointer flex '>
-            Alexander &nbsp;
-            <span className='sm:block hidden'> | software developer </span>
+            <span className='sm:block hidden'> Alexander | Software Developer </span>
           </p>
         </Link>
 
